73264d33a85707ee92ddb5a8ae8be5eda6581b3b
ncarnarius <39972269+ncarnarius@users.noreply.github.com>
2018-07-02 11:19:17 -0700

    Update mycode.js

--- mycode.js
+++ mycode.js
@@ -1,7 +1,7 @@
 $(function() {
   // word switcher
   const switcher = $('#word-switcher');
-  const delay = 2000;
+  const delay = 2500;
   const count = switcher.find('p').length;
 
   const calculateWidths = function() {
@@ -32,3 +32,86 @@ $(function() {
   });
   setTimeout(doChange, delay);
 });
+
+
+
+// --------------- scroll animation 1 ------------------- //
+
+(function($) {
+
+  /**
+   * Copyright 2012, Digital Fusion
+   * Licensed under the MIT license.
+   * http://teamdf.com/jquery-plugins/license/
+   *
+   * @author Sam Sehnert
+   * @desc A small plugin that checks whether elements are within
+   *     the user visible viewport of a web browser.
+   *     only accounts for vertical position, not horizontal.
+   */
+
+  $.fn.visible = function(partial) {
+    
+      var $t            = $(this),
+          $w            = $(window),
+          viewTop       = $w.scrollTop(),
+          viewBottom    = viewTop + $w.height(),
+          _top          = $t.offset().top,
+          _bottom       = _top + $t.height(),
+          compareTop    = partial === true ? _bottom : _top,
+          compareBottom = partial === true ? _top : _bottom;
+    
+    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+  };
+    
+})(jQuery);
+
+var win = $(window);
+
+var allMods = $(".module1");
+
+allMods.each(function(i, el) {
+  var el = $(el);
+  if (el.visible(true)) {
+    el.addClass("already-visible"); 
+  } 
+});
+
+win.scroll(function(event) {
+  
+  allMods.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("come-in"); 
+    } 
+  });
+  
+});
+
+
+// ----------------- items scroll up ------------------ //
+
+$('.revealedBox').each(function(i){ 
+
+	var childrenSpan = $(this).children('span').length;
+
+	$(this).addClass('childrenSpan-' + childrenSpan);  
+
+  if($(window).scrollTop() + $(window).height() > $(this).offset().top + $(this).outerHeight() ){ 
+  	$(this).addClass('revealedBox-in');       
+  }   
+
+}); 
+
+$(window).scroll(function() { 
+	$('.revealedBox').each(function(i){  
+  if($(window).scrollTop() + $(window).height() > $(this).offset().top ){ 
+  	$(this).addClass('revealedBox-in');       
+  }   
+}); 
+  
+});
+
+
+
